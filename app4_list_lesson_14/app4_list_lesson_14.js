6a611a4a562d8704d0fd78bcf8066d01506bbf74
Josh Seipel <joshseipel@gmail.com>
2022-08-14 13:11:27 -0400

    Updated with comments.

--- app4_list_lesson_14/app4_list_lesson_14.js
+++ app4_list_lesson_14/app4_list_lesson_14.js
@@ -1,20 +1,27 @@
-console.log("app4_list_lesson_14.js is ready!")
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log(event)
+    // DOMContentLoaded - Ensures that the DOM has been loaded entirely
+    // without waiting for images, stylesheets.
+    // TODO: Create a function to check the state of the DOM.
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
+    console.log("app4_list_lesson_14.js is ready!")
+    const output = document.querySelector(".output");
+    console.log(output)
 
-const jsonUrl = "/app4_list_lesson_14/app4_list_lesson_14.json";
-let data = "";
+    const jsonUrl = "/app4_list_lesson_14/app4_list_lesson_14.json";
 
-function getJson() {
-    console.log("getJson data")
+    let data = "";
 
-    fetch(jsonUrl)
-    .then(resp => resp.text())
-    .then(jsonData => {
-        data = JSON.parse(jsonData)
-        console.log(jsonData)
-        console.log(data)
-    })
+    function getJson() {
+        fetch(jsonUrl)
+        .then(resp => resp.text())
+        .then(jsonData => {
+            data = JSON.parse(jsonData)
+            console.log(data)
+        })
+    }
 
-    console.log("my new data", data)
-}
+    getJson();
+});
 
-getJson();
+
