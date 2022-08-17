ee455bdcf39b53631d2203f83851e46bbb512ca8
Josh Seipel <joshseipel@gmail.com>
2022-08-17 11:36:12 -0400

    Fixed makeList output scope bug.

--- app4_list_lesson_14/app4_list_lesson_14.js
+++ app4_list_lesson_14/app4_list_lesson_14.js
@@ -4,19 +4,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // TODO: Create a function to check the state of the DOM.
     // https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
     console.log("app4_list_lesson_14.js is ready!")
+    
     const jsonUrl = "/app4_list_lesson_14/app4_list_lesson_14.json";
     const output = document.querySelector(".output");
-    output.textContent = "New Content:"
+    output.textContent = "Loading...";
 
     function getJson() {
         fetch(jsonUrl)
         .then(resp => resp.json())
         .then(jsonData => {
             console.log(jsonData)
+            output.innerHTML = "";
+            jsonData.forEach(elm => {
+                makeList(elm);
+            })
         })
     }
 
     getJson();
+
+    function makeList(item){
+        const div = document.createElement("div");
+        div.innerHTML = `User: ${item.first_name} ${item.last_name}`;
+        output.append(div);
+    }
+
+
+
+
+
 });
 
 
