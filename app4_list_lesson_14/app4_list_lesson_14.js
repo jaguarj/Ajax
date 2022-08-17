f746306081b4c82f4463af4cd8b053055d1d22cc
Josh Seipel <joshseipel@gmail.com>
2022-08-17 11:11:40 -0400

    Updated: General house keeping.

--- app4_list_lesson_14/app4_list_lesson_14.js
+++ app4_list_lesson_14/app4_list_lesson_14.js
@@ -1,23 +1,18 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log(event)
     // DOMContentLoaded - Ensures that the DOM has been loaded entirely
     // without waiting for images, stylesheets.
     // TODO: Create a function to check the state of the DOM.
     // https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
     console.log("app4_list_lesson_14.js is ready!")
-    const output = document.querySelector(".output");
-    console.log(output)
-
     const jsonUrl = "/app4_list_lesson_14/app4_list_lesson_14.json";
-
-    let data = "";
+    const output = document.querySelector(".output");
+    output.textContent = "New Content:"
 
     function getJson() {
         fetch(jsonUrl)
-        .then(resp => resp.text())
+        .then(resp => resp.json())
         .then(jsonData => {
-            data = JSON.parse(jsonData)
-            console.log(data)
+            console.log(jsonData)
         })
     }
 
